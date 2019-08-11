e04153e7b4344d001f75ba7ddff9444333dcea3c
ThundaHorse <abeeeeekim@gmail.com>
2019-08-10 21:05:59 -0400

    added todos in state

---
+++ src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>Todos</h3>
+    <div class="todos">
+      <div v-for="todo in allTodos" :key="todo.id" class="todo">{{ todo.title }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+// This maps getters from Vuex to the component as an array
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Todos",
+  // Way that we implement or define what getters we want to use
+  // Allows use of getter which returns the state desired
+  computed: mapGetters(["allTodos"])
+};
+</script>
+
+<style>
+</style>
