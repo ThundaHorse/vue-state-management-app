4a5ede4b3e17a3137736e273c9231620d020f3cb
ThundaHorse <abeeeeekim@gmail.com>
2019-08-13 22:24:52 -0400

    configuring netlify

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,10 @@ import Vuex from 'vuex';
 import Vue from 'vue';
 import todos from './modules/todos';
 
-// Load Vuex 
+// Load Vuex
 Vue.use(Vuex);
 
-// Create the store 
+// Create the store
 export default new Vuex.Store({
   modules: {
     todos
